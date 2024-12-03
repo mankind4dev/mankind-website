044ed626e4a5104efcb28d77d44e54fdc38e2e1d
mankind4dev <ogunmakin120@gmail.com>
2024-12-03 16:05:50 +0100

    create header section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+import { Button } from "flowbite-react";
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='text-red-300'>Home Page</div>
-  )
-}
+    <>
+      <div className="text-red-300">Home Page</div>
+      <Button gradientDuoTone="purpleToBlue" pill outline>ljukfl,'</Button>
+    </>
+  );
+};
 
-export default Home
+export default Home;
